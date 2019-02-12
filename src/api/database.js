eb8bb64360e53e085944f860d9424f0ca0f7ab50
Aldren Terante <aldren.terante@gmail>
2019-02-12 12:10:15 +0800

    add premock data in localstorage using lowdb

---
+++ src/api/database.js
@@ -0,0 +1,82 @@
+import shortid from 'shortid';
+import low from 'lowdb';
+import LocalStorage from 'lowdb/adapters/LocalStorage';
+
+const adapter = new LocalStorage('db');
+export const db = low(adapter);
+
+const initMockData = () => {
+  db.defaults({
+    posts: [{
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }, {
+      id: shortid.generate(),
+      title: 'post title',
+      content: 'post content',
+    }]
+  })
+  .write();
+}
+
+const isEmptyState = Object.entries(db.getState()).length === 0 && db.getState().constructor === Object;
+if (isEmptyState) {
+  initMockData();
+}
